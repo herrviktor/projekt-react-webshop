4cd130a137f954f736c82ab91246307698050bad
Viktor Ekstrom <herrviktor@hotmail.com>
2024-06-08 13:35:40 +0200

    En ostylad header läggs till

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import './App.css';
+import Header from './komponenter/header.jsx'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">Tall & Thin</h1>
-        <p>
-          Ckothes for people who are tall but not fat
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Varukorg
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
---
+++ src/komponenter/header.jsx
@@ -0,0 +1,12 @@
+import './header.css';
+        export default function Header() {
+            return ( 
+                <header className="header"> 
+                    <h1>
+                        TALL & THIN<span>Clothes for people who are tall but not fat</span>
+                    </h1>
+                    
+                </header>
+            );
+        }
+        
